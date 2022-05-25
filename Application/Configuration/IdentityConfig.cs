ec657d2c27fe9555c0755e0ad7da5038a0aaaa38
Michel Micheloti <michel.michelot@hotmail.com>
2022-05-25 14:25:52 -0300

    teste

--- Application/Configuration/IdentityConfig.cs
+++ Application/Configuration/IdentityConfig.cs
@@ -22,30 +22,31 @@ namespace Application.Configuration
                 .AddDefaultTokenProviders();
 
             // JWT
-            // IConfigurationSection appSettingsSection = configuration.GetSection("AppSettings");
-            // services.Configure<AppSettings>(appSettingsSection);
+            IConfigurationSection appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
 
-            // AppSettings appSettings = appSettingsSection.Get<AppSettings>();
-            // byte[] key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            AppSettings appSettings = appSettingsSection.Get<AppSettings>();
+            byte[] key = Encoding.ASCII.GetBytes(appSettings.Secret);
 
-            // services.AddAuthentication(options =>
-            // {
-            //     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            // }).AddJwtBearer(bearerOptions =>
-            // {
-            //     bearerOptions.RequireHttpsMetadata = true;
-            //     bearerOptions.SaveToken = true;
-            //     bearerOptions.TokenValidationParameters = new TokenValidationParameters
-            //     {
-            //         ValidateIssuerSigningKey = true,
-            //         IssuerSigningKey = new SymmetricSecurityKey(key),
-            //         ValidateIssuer = true,
-            //         ValidateAudience = true,
-            //         ValidAudience = appSettings.Audience,
-            //         ValidIssuer = appSettings.Issuer
-            //     };
-            // });
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(bearerOptions =>
+            {
+                bearerOptions.RequireHttpsMetadata = true;
+                bearerOptions.SaveToken = true;
+                bearerOptions.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.Audience,
+                    ValidIssuer = appSettings.Issuer
+                };
+            });
+            
             // byte[] key = Encoding.ASCII.GetBytes(configuration["Authentication:Secret"]);
 
             // services.AddAuthentication(options =>
@@ -69,9 +70,9 @@ namespace Application.Configuration
 
             // Ativa o uso do token como forma de autorizar o acesso
             // a recursos deste projeto
-            // services.AddAuthorization(auth => auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
-            //         .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme‌​)
-            //         .RequireAuthenticatedUser().Build()));
+            services.AddAuthorization(auth => auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme‌​)
+                    .RequireAuthenticatedUser().Build()));
 
             return services;
         }
